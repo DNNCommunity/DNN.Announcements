6609a81b1cc9ef573e53b5eb679f481a5728bc76
Daniel <info@danielvaladas.com>
2025-04-02 14:35:42 -0400

    Implemented nuget restore

--- build/Build.cs
+++ build/Build.cs
@@ -7,9 +7,11 @@ using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 [GitHubActions("CI",
     GitHubActionsImage.Ubuntu2204,
@@ -18,7 +20,6 @@ using static Nuke.Common.IO.PathConstruction;
     FetchDepth = 0,
     OnPullRequestBranches = new[] { "*" },
     OnPushBranches = new[] { "develop", "main", "master", "release/*" },
-    Progress = true,
     PublishArtifacts = true)]
 class Build : NukeBuild
 {
@@ -33,21 +34,29 @@ class Build : NukeBuild
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
+    [Solution] readonly Solution Solution;
+
+    // DIRECTORIES
+    AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+
     Target Clean => _ => _
         .Before(Restore)
         .Executes(() =>
         {
+            ArtifactsDirectory.CreateOrCleanDirectory();
         });
 
     Target Restore => _ => _
         .Executes(() =>
         {
+            DotNetRestore(s => s
+                .SetProjectFile(Solution));
         });
 
     Target Compile => _ => _
         .DependsOn(Restore)
         .Executes(() =>
         {
-        });
 
+        });
 }
