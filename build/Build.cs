3d01f2e1f8db70f396bcb0fb02ca93f0a749b72e
Daniel <info@danielvaladas.com>
2025-04-02 16:39:20 -0400

    Implemented automatic versioning

--- build/Build.cs
+++ build/Build.cs
@@ -5,15 +5,19 @@ using Nuke.Common;
 using Nuke.Common.CI;
 using Nuke.Common.CI.GitHubActions;
 using Nuke.Common.Execution;
+using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
 using Nuke.Common.Tools.MSBuild;
 using Nuke.Common.Tools.NuGet;
 using Nuke.Common.Tools.Xunit;
+using Nuke.Common.Utilities;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.XmlTasks;
 using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 using static Nuke.Common.Tools.MSBuild.MSBuildTasks;
@@ -43,12 +47,14 @@ class Build : NukeBuild
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
     [Solution] readonly Solution Solution;
+    [GitRepository] readonly GitRepository GitRepository;
+    [GitVersion] readonly GitVersion GitVersion;
 
     // DIRECTORIES
     AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
     AbsolutePath SourcesDirectory => RootDirectory / "src";
     AbsolutePath DeploymentDirectory => RootDirectory.Parent / "Announcements";
-    AbsolutePath WebsiteDirectory = RootDirectory.Parent.Parent;
+    AbsolutePath WebsiteDirectory => RootDirectory.Parent.Parent;
 
     // Files
     AbsolutePath ModuleAssembly => SourcesDirectory / "bin" / "DotNetNuke.Modules.Announcements.dll";
@@ -79,11 +85,20 @@ class Build : NukeBuild
 
     Target Compile => _ => _
         .DependsOn(Restore)
+        .DependsOn(SetVersion)
         .Executes(() =>
         {
             MSBuild(s => s
                 .SetConfiguration(Configuration)
-                .SetProjectFile(Solution));
+                .SetProjectFile(Solution)
+                .SetAssemblyVersion(GitVersion.MajorMinorPatch));
+        });
+
+    Target SetVersion => _ => _
+        .Executes(() =>
+        {
+            Serilog.Log.Information($"Version: {GitVersion.ToJson()}");
+            SetManifestVersion();
         });
 
     Target Test => _ => _
@@ -117,6 +132,19 @@ class Build : NukeBuild
                     Serilog.Log.Information($"Copying {file} to {destination}");
                     file.CopyToDirectory(destination, ExistsPolicy.MergeAndOverwrite);
                 });
+        });
+    }
+
+    private void SetManifestVersion()
+    {
+        var manifests = SourcesDirectory.GlobFiles("*.dnn");
+        manifests.ForEach(manifest =>
+        {
+            XmlPoke(
+                manifest,
+                "//packages/package/@version",
+                GitVersion.MajorMinorPatch);
+            Serilog.Log.Information($"Updated version in {manifest} manifest file to {GitVersion.MajorMinorPatch}");
         });
     }
 }
